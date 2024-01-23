0fb238b05363f0bb71d9f0e98dc90cd527c096c4
Kairatto <KairattoSS@gmail.com>
2024-01-24 01:41:05 +0600

    fix urls

--- apps/location_info/urls.py
+++ apps/location_info/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from apps.location_info.views import LocationInfoCreate, LocationInfoDetail, LocationInfoList
+from apps.location_info.views import LocationInfoCreate, LocationInfoDetail, LocationInfoList, LocationInfoDevList
 
 
 urlpatterns = [
     path('create/', LocationInfoCreate.as_view(), name='location-create'),
     path('list/', LocationInfoList.as_view(), name='location-list'),
+    path('list_dev/', LocationInfoDevList.as_view(), name='location-list-dev'),
     path('<str:slug>/', LocationInfoDetail.as_view(), name='location-detail'),
 ]
