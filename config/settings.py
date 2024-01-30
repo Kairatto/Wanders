cb6ae1c0f4475348fe6c53ff6f1abcaaeba5acb7
Kairatto <113059094+Kairatto@users.noreply.github.com>
2024-01-30 21:57:09 +0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -207,7 +207,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = config('EMAIL_PORT', default = 587)
 EMAIL_HOST_USER = 'Kairattoss@gmail.com'
-EMAIL_HOST_PASSWORD = 'KairattoSS135790'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 
 
